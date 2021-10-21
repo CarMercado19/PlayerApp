d3edf377562532925f43f989d878cc06fb7e342c
CarMercado19 <88337802+CarMercado19@users.noreply.github.com>
2021-10-21 15:53:43 -0500

    probando vscode web

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import Slidebar from './components/Slidebar/Slidebar';
 import Main from './components/Main/Main';
 
 const App = () => {
-  return (
-    <div className="outerWrap">
-      <div className="App">
-        <Slidebar />
-        <Main />
-      </div>
-      <div className="musicControls">
-        Previous/Play/Next
-      </div>
-    </div>
-  );
+    return (
+        <div className="outerWrap">
+            <div className="App">
+                <Slidebar />
+                <Main />
+            </div>
+            <div className="musicControls">
+                Previous/Play/Next
+            </div>
+        </div>
+    );
 }
 
 export default App;
